4fd668a287fcf0e98d5ac51a2e2730ae88470219
LimPark996 <uji835311@gmail.com>
2025-09-17 12:58:06 +0900

    fix: concerts 엔티티의 max_tickets_per_user 컬럼 삭제(1인당 최대 2개의 좌석까지 예매 가능한 점 고려했을때, 해당 컬럼은 실제로 반영이 어려움)

--- src/main/java/com/team03/ticketmon/concert/domain/Concert.java
+++ src/main/java/com/team03/ticketmon/concert/domain/Concert.java
@@ -88,10 +88,6 @@ public class Concert extends BaseTimeEntity {
 	@Builder.Default
 	private Integer minAge = 0;
 
-	@Column(name = "max_tickets_per_user", nullable = false)
-	@Builder.Default
-	private Integer maxTicketsPerUser = 4;
-
 	// ENUM을 문자열로 저장 (ORDINAL 사용 금지)
 	@Enumerated(EnumType.STRING)
 	@Column(length = 20, nullable = false)
