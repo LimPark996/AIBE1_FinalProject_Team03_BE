58adbda0abad42f9986fd9c87b85a68a13b7cb24
LimPark996 <uji835311@gmail.com>
2025-10-20 16:35:03 +0900

    fix: VenueDTO에서 final 및 생성자 문제 해결 -> Redis 캐시에 반영됨

--- src/main/java/com/team03/ticketmon/venue/dto/VenueDTO.java
+++ src/main/java/com/team03/ticketmon/venue/dto/VenueDTO.java
@@ -1,15 +1,19 @@
 package com.team03.ticketmon.venue.dto;
 
 import com.team03.ticketmon.venue.domain.Venue;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 공연장 정보 전송을 위한 DTO(Data Transfer Object)들을 담는 클래스
  */
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class VenueDTO {
-    private final Long venueId;
-    private final String name;
+    private Long venueId;
+    private String name;
 
     public VenueDTO(Venue venue) {
         this.venueId = venue.getVenueId();
